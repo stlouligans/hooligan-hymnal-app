a3e0f96cc0c0c228fb675b1a9066762fef2f0d63
Matt H <44037139+Mbwata@users.noreply.github.com>
2023-03-02 15:55:52 -0600

    update for android builds

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
 module.exports = () => {
   if (process.env.APP_ENV === "production") {
     return require("./app.production.json");
-  } else if (process.env.APP_ENV === "staging") {
-    return require("./app.staging.json");
+  // } else if (process.env.APP_ENV === "staging") {
+  //   return require("./app.staging.json");
   } else {
     return require("./app.development.json");
   }
